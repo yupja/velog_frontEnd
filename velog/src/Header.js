74e9f059c022ea32371f0953dca9b39b23f86ebd
sparrowscout <sparrowscout.0000@gmail.com>
2022-06-18 17:34:40 +0900

    헤더 부분에 모달 창 추가

--- velog/src/Header.js
+++ velog/src/Header.js
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components"
+import Login from "./Login";
+
 const Header = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => {
+        setShowModal(true)
+    }
+
+    const closeModal = () => {
+        setShowModal(false);
+    }
+
+
     return (
-        null
+        <Wrap>
+            <HeaderContainer>
+                <Link to="/"> 홈으로 가기</Link>
+                <span onClick={openModal}>로그인</span>
+                {showModal ?
+                    <Login showModal={showModal} closeModal={closeModal} />
+                    : null}
+            </HeaderContainer>
+        </Wrap>
+
     );
 }
+
+const Wrap = styled.div``;
+
+const HeaderContainer = styled.header`
+background-color: aliceblue;
+`;
+
 
 export default Header;
