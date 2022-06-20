ccf149a7fd466274f5fb59dd40cbd81db5317256
sparrowscout <sparrowscout.0000@gmail.com>
2022-06-20 16:46:24 +0900

    기타 수정 (기능x)

--- velog/src/Detail.js
+++ velog/src/Detail.js
@@ -1,7 +1,46 @@
+import axios from "axios";
+import React, { useEffect } from "react"
+import styled from "styled-components";
+
 const Detail = () => {
+    const axios = require('axios').default;
+
+    var post = null;
+
+    useEffect(() => {
+        getBoard()
+    }, [])
+
+
+    const getBoard = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/GetBoardDetail');
+            console.log(response.data[0]);
+            return post = response.data[0]
+        } catch (error) {
+            console.log(error)
+        }}
+
+        console.log(post)
     return (
-        null
+        <DetailContainer>
+            <h1>디테일 페이지 입니다</h1>
+            <span>닉네임 </span>
+            작성일자<br />
+            {post}
+            tag1 tag2 tag3<br />
+            썸네일<br />
+            내용
+            <br />
+            0개의 댓글<br />
+            <textarea typeof="text" placeholder="댓글을 작성하세요" />
+            <button>댓글 작성</button>
+        </DetailContainer>
+
     );
 }
+
+const DetailContainer = styled.div`
+`;
 
 export default Detail;
--- velog/src/Main.js
+++ velog/src/Main.js
@@ -1,7 +1,18 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
 const Main = () => {
     return (
-        null
+        <>
+        <h1>메인 페이지 입니다</h1>
+        <Link to="/detail">
+        <div style={{width:"100px", height:"100px", backgroundColor:"wheat"}}>게시글 1</div>
+       </Link>
+        </>
+        
     );
 }
+
+
 
 export default Main;
